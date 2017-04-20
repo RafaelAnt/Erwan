3a7dd531c7dc0df9e6ef95d2bb5b066ba6b06252
Rafael Antunes <rafael.uminho@hotmail.com>
2017-04-20 11:56:13 +0100

    Init

---
+++ Erwan/app/src/main/java/rafaelantunes/erwan/activities/SpalshScreenActivity.java
@@ -0,0 +1,15 @@
+package rafaelantunes.erwan.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import rafaelantunes.erwan.R;
+
+public class SpalshScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh_screen);
+    }
+}
